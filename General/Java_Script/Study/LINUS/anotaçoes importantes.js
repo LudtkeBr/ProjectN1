865f29241a22fd8a8849d2efc0bb1b5172c2105e
LINUSPUTODAVIDA <linus.kienen@gmail.com>
2020-05-07 19:30:54 -0300

    Irei postar cada vez mais sobre javascript, e aproveite e duvidas, falar com Linus

---
+++ General/Java_Script/Study/LINUS/anotaçoes importantes.js
@@ -0,0 +1,8 @@
+//==compara o valor de duas expressoes
+//"" quer dizer texto, string
+//!== compara se valor e tipos sao diferentes
+//!= compara se os valores sao diferentes
+//&& para poder comparar duas condiçoes, tem que ser verdadeiras, caso uma seja e aoutra nao, toda sentença é ignorada
+//|| "or" compara se uma é verdade ou as aduas
+
+//if(condiçao){}
